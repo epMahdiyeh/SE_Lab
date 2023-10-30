a4c77333dd532d815a1823025750830357413ed8
Mahdiyeh <mahdiyeh.ep@gmail.com>
2023-10-30 13:28:18 +0330

    calculator menu options added

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#functions
+
+options = {
+    "1": "Add",
+    "2": "Subtract",
+    "3": "Multiply",
+    "4": "Divide",
+    "5": "Power",
+    "6": "Root",
+    "7": "Square Root",
+    "8": "Exit"
+}
