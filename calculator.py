a66745b4abc6cd3abcebf3b8a9867007a2184afd
dariusamiri <dariush_amiri7@yahoo.com>
2023-10-30 20:32:37 +0330

    Added subtract function

--- calculator.py
+++ calculator.py
@@ -3,9 +3,12 @@ import math
 class Calculator:
     def __init__(self):
         self.result = 0
-        
+
     def add(self, x, y):
         self.result = x + y
+
+    def subtract(self, x, y):
+        self.result = x - y
 
 
 calc = Calculator()
